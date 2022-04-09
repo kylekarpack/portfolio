016d60b8a19cd66e89752df0494fb5312e594262
Kyle Karpack <kylekarpack@gmail.com>
2022-04-09 16:59:02 -0700

    Add basic MDX

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,13 +1,14 @@
 import { Link } from "@remix-run/react";
-import { FunctionComponent } from "react";
+import type { FunctionComponent } from "react";
 
 export const Nav: FunctionComponent = () => {
-
-	return <nav>
-		<Link to="/">Home</Link>
-		<Link to="/about">About</Link>
-		<Link to="/about">Resume</Link>
-		<Link to="/portfolio">Portfolio</Link>
-		<Link to="/contact">Contact</Link>
-	</nav>;
-}
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/about">Resume</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+};
--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,6 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
   viewport: "width=device-width,initial-scale=1",
 });
 
