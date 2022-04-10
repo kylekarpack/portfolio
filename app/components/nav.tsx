85e9a899aebffd3d53481ceb709617429329968a
Kyle Karpack <kylekarpack@gmail.com>
2022-04-09 17:01:58 -0700

    Add resume page

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,7 +6,7 @@ export const Nav: FunctionComponent = () => {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/about">Resume</Link>
+      <Link to="/resume">Resume</Link>
       <Link to="/portfolio">Portfolio</Link>
       <Link to="/contact">Contact</Link>
     </nav>
