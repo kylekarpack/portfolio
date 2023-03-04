218a8551a49878ea221030e79261c594beacff86
Kyle Karpack <kylekarpack@gmail.com>
2023-03-04 09:53:55 -0800

    Resume updates

--- app/data/details.ts
+++ app/data/details.ts
@@ -5,23 +5,6 @@ export interface Data {
   url: string;
 }
 
-export const data: Record<string, string> = {
-  default: `This is a <b class="font-bold">non exhaustive</b> list of tools and technologies I use to make products come to life. I'm also very comfortable pushing pixels in Figma, Adobe, Video, and 3D modelling.`,
-  TypeScript: `TypeScript is a programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. It is designed for the development of large applications and transpiles to JavaScript.`,
-  Remix: `Remix is a full stack web framework that lets you focus on the user interface and work back through web standards to deliver a fast, slick, and resilient user experience. People are gonna love using your stuff.`,
-  React: `React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.`,
-  "Node.js": `Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.`,
-  TailwindCSS: `TailwindCSS is a framework like no other. Rather than constraining you to a set design, it gives you the tools and the standardization to build exactly what you want.`,
-  DevOps: `Working on small(er) teams has allowed me to venture out of my comfort zone and into fields like DevOps. The opportunity to impact the Developer experience every step of the way has been incredibly rewarding.`,
-  Docker: `A microservice architecture encourages the use of many application working in tandem. Using Docker and Docker Compose we can quickly spin up very complex systems mirroring what we use in production.`,
-  Javascript: `I've been using Javascript for well over a decade and I've only continued to find myself more excited about the language and accellerated growth it has seen.`,
-  Kubernetes: `Running an organization like thredUP at scale is a great way to learn about the Kubernetes ecosystem. I've been using Kubernetes for a while now and I'm very excited to learn more about it in the future.`,
-  NextJS: `Next.js is an open-source web development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.`,
-  PostCSS: `PostCSS is a software development tool that uses JavaScript-based plugins to automate routine CSS operations. A tool for transforming CSS with JavaScript`,
-  VSCode: `Visual Studio Code, also commonly referred to as VS Code, is a source-code editor made by Microsoft. Features include support for debugging, syntax highlighting, intelligent code completion, snippets, code refactoring, and embedded Git. It rocks 🤘!`,
-  CSS: `Pushing pixels is what got me into development so its no suprise I genuinely love CSS. My preferred tooling includes; TailwindCSS, CSS Modules, PostCSS, and Progressive Enhancement.`,
-};
-
 export const technologies: Record<string, string[]> = {
   Languages: [
     "TypeScript",
@@ -42,16 +25,8 @@ export const technologies: Record<string, string[]> = {
     "Gatsby",
     "GraphQL",
     "Recoil",
+    ".NET",
   ],
-  "Infrastructure & Devops": [
-    "Azure",
-    "AWS",
-    "Jenkins",
-    "GitHub Actions",
-    "Docker",
-  ],
-  Testing: ["Jest", "Testing Library", "Vitest", "Cypress"],
-  Monitoring: ["Splunk", "New Relic", "Grafana"],
   Tooling: [
     "Webpack",
     "Parcel",
@@ -63,6 +38,15 @@ export const technologies: Record<string, string[]> = {
     "Git",
     "Jira",
   ],
+  "Infrastructure & Devops": [
+    "Azure",
+    "AWS",
+    "Jenkins",
+    "GitHub Actions",
+    "Docker",
+  ],
+  Testing: ["Jest", "Testing Library", "Vitest", "Cypress"],
+  Monitoring: ["Splunk", "New Relic", "Grafana"],
   Databases: ["Snowflake", "MSSQL", "MongoDB", "Postgres", "ElasticSearch"],
   Design: ["Various Adobe products", "SVG", "image optimization"],
 };
