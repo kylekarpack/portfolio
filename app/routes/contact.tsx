fc7a63652f37b1ba35c15d9d8a43a1e42eb9177a
Kyle Karpack <kylekarpack@gmail.com>
2023-03-05 07:11:38 -0800

    Basic map addition

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,9 @@
 import { MetaFunction } from "@remix-run/server-runtime";
+import { useState } from "react";
 import { SITE_AUTHOR, SITE_TITLE } from "~/config/constants";
 import { getMetaData } from "~/metadata";
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export const meta: MetaFunction = (args) => {
   return {
@@ -13,5 +16,26 @@ export const meta: MetaFunction = (args) => {
 };
 
 export default function () {
-  return <div>Contact me</div>;
+  const [map, setMap] = useState({
+    mapboxApiAccessToken:
+      "pk.eyJ1Ijoia3lsZWthcnBhY2siLCJhIjoiY2pvZXZmNTh4MDZ2dzN3bm1pbmk1dDlmZiJ9.Gapqs5j98RUsHOBl2rqOGQ",
+    mapStyle: "mapbox://styles/mapbox/outdoors-v10",
+    viewport: {
+      width: "100%",
+      height: "40vh",
+      latitude: 47.6798,
+      longitude: -122.3258,
+      zoom: 11,
+    },
+  });
+  return (
+    <div>
+      <ReactMapGL
+        mapboxAccessToken={map.mapboxApiAccessToken}
+        mapStyle={map.mapStyle}
+        onViewportChange={(viewport) => setMap({ ...map, viewport })}
+        {...map.viewport}
+      />
+    </div>
+  );
 }
