a0832c907c700d7856f4a6662cd769706887378f
Kyle Karpack <kylekarpack@gmail.com>
2023-03-11 09:22:47 -0800

    Add back images

--- app/components/AppHeaderMobile.tsx
+++ app/components/AppHeaderMobile.tsx
@@ -60,14 +60,14 @@ export const AppHeaderMobile = () => {
         <div className="flex-1 md:hidden" />
 
         <NavLink className="header-logo ml-4 flex h-full items-center gap-2 px-4" to="/">
-          <AppLogo className="h-full" />
+          <AppLogo />
           <span className="sr-only">Home</span>
         </NavLink>
 
         <div className="flex-1 pl-20" />
       </div>
 
-      <div className="h-full font-font-monospace md:flex-row">{!closed && renderNavigation()}</div>
+      <div className="h-full md:flex-row">{!closed && renderNavigation()}</div>
     </header>
   );
 };
--- app/components/AppLogo.tsx
+++ app/components/AppLogo.tsx
@@ -1,9 +1,5 @@
-export interface AppLogoProps {
-  className?: string;
-  fill?: string;
-  height?: number;
-}
-
-export const AppLogo = (props: AppLogoProps) => {
-  return <img alt="Toggle Menu" loading="eager" style={{ height: "32px" }} src="/images/branding/logo.svg" />;
+export const AppLogo = () => {
+  return (
+    <img alt="Toggle Menu" loading="eager" style={{ height: "32px" }} height={32} src="/images/branding/logo.svg" />
+  );
 };
