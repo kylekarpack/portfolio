0e082785d5e381d5e42fe6626dcbe4fa6f844085
Kyle Karpack <kylekarpack@gmail.com>
2023-03-12 15:09:14 -0700

    Build updates

--- remix.config.js
+++ remix.config.js
@@ -1,17 +1,18 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // When running locally in development mode, we use the built in remix server
-  server:
-    process.env.NODE_ENV === "production"
-      ? // when running the Netify CLI or building on Netlify, we want to use
-        {
-          server: "./server.js",
-          serverBuildPath: ".netlify/functions-internal/server.js",
-        }
-      : // otherwise support running remix dev, i.e. no custom server
-        undefined,
+  ...baseConfig,
   ignoredRouteFiles: [".*"],
   future: {
     unstable_tailwind: true,
