40ad5599ffe0ae5b4ec5632521671f581a363160
Kyle Karpack <kylekarpack@gmail.com>
2024-12-26 13:32:06 -0800

    Netlify fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
@@ -15,5 +16,6 @@ export default defineConfig({
       ignoredRouteFiles: ["**/*.css"],
     }),
     tsconfigPaths(),
+    netlifyPlugin(),
   ],
 });
